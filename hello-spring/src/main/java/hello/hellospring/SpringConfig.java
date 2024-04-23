fb4329f0cc5129481b7d3d013d26435b786bf367
김준혁 <skdhd999@gmail.com>
2024-04-23 21:46:16 +0900

    스프링 데이터 JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -17,23 +17,23 @@ import jakarta.persistence.EntityManager;
 @Configuration
 public class SpringConfig {
 
-	private EntityManager em;
+	private final MemberRepository memberRepository;
 	
 	@Autowired
-	public SpringConfig(EntityManager em) {
-		this.em = em;
-	}
-	
-	@Bean
-	public MemberService memberService() {
-		return new MemberService(memberRepository());
+	public SpringConfig(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
 	}
 
 	@Bean
-	public MemberRepository memberRepository() {
-		//return new MemoryMemberRepository();
-		//return new JdbcMemberRepository(dataSource);
-		//return new JdbcTemplateMemberRepository(dataSource);
-		return new JpaMemberRepository(em);
+	public MemberService memberService() {
+		return new MemberService(memberRepository);
 	}
+
+//	@Bean
+//	public MemberRepository memberRepository() {
+//		//return new MemoryMemberRepository();
+//		//return new JdbcMemberRepository(dataSource);
+//		//return new JdbcTemplateMemberRepository(dataSource);
+//		return new JpaMemberRepository(em);
+//	}
 }
