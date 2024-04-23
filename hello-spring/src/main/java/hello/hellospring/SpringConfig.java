a487a909eec490efeb1e08a30b1f5218b9e7c45e
김준혁 <skdhd999@gmail.com>
2024-04-23 19:48:09 +0900

    JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -8,29 +8,32 @@ import org.springframework.context.annotation.Configuration;
 
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-	private DataSource dataSource;
+	private EntityManager em;
 	
 	@Autowired
-	public SpringConfig(DataSource dataSource) {
-		this.dataSource = dataSource;
+	public SpringConfig(EntityManager em) {
+		this.em = em;
 	}
 	
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
 	}
-	
+
 	@Bean
 	public MemberRepository memberRepository() {
 		//return new MemoryMemberRepository();
 		//return new JdbcMemberRepository(dataSource);
-		return new JdbcTemplateMemberRepository(dataSource);
+		//return new JdbcTemplateMemberRepository(dataSource);
+		return new JpaMemberRepository(em);
 	}
 }
